b3923d43370dea4001589a63f3347172be321b0b
JHawi13 <Joherwi@gmail.com>
2023-03-31 09:52:10 +0300

    Update

---
+++ arduino_robotics/arduino_robotics.ino
@@ -0,0 +1,17 @@
+// motor a
+int ena = 33;
+int in1 = 32;
+int in2 = 35;
+//motor b
+int enb = 36;
+int in3 = 34;
+int in4 = 39;
+void setup() {
+  // put your setup code here, to ru once:
+  // set your GPIO(PINS)
+ Serial.begin(115200); //used to set thee speed of communication of the controller(circuit board) in bits.
+
+}
+void loop() {
+
+}
